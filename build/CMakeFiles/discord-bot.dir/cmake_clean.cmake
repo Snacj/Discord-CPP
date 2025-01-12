90682726dc773800e44a2ee71aa30e903e0c9bb4
snacj <bbmelvin@gmx.de>
2025-01-12 17:58:55 +0100

    Simple POC program of c++ discord bot using D++.

---
+++ build/CMakeFiles/discord-bot.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/discord-bot.dir/link.d"
+  "CMakeFiles/discord-bot.dir/src/main.cpp.o"
+  "CMakeFiles/discord-bot.dir/src/main.cpp.o.d"
+  "discord-bot"
+  "discord-bot.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/discord-bot.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
